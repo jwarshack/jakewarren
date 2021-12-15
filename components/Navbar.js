81a2176286c4f8d7d8955852a6e494dabe0156f5
Jake Warren <jwarshack@gmail.com>
2021-12-14 20:09:27 -0800

    react scroll

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import {BsGithub, BsTwitter, BsMedium} from 'react-icons/bs'
-import Link from 'next/link'
+import NextLink from 'next/link'
+import { Link, animateScroll as scroll } from "react-scroll";
 
 export default function Navbar() {
     return (
         <div className='flex justify-between items-center px-10 py-7' style={{backgroundColor: "#99BF72"}}>
-            <Link href="/#projects">Projects</Link>
+            <Link to="projects" smooth={true} className='cursor-pointer'>Projects</Link>
             <div className='flex text-2xl md:text-4xl space-x-4 md:space-x-6'>
                 <a href='https://twitter.com/jwarshack' target="_blank" rel="noopener noreferrer"><BsTwitter/></a>
                 <a href='https://github.com/jwarshack' target="_blank" rel="noopener noreferrer"><BsGithub/></a>
                 <a href='https://medium.com/@thelasthash' target="_blank" rel="noopener noreferrer"><BsMedium/></a>
             </div>
-            <Link href="/#contact">Contact</Link>
+            <Link to="contact" smooth={true} className='cursor-pointer'>Contact</Link>
         </div>
     )
 }
